53067ca74d2c350f8176f67246a783220276d0dd
Anusha2 <asriren@ilstu.edu>
2017-07-07 11:19:29 -0500

    added a statement in print

--- MyProject1/src/myclass.java
+++ MyProject1/src/myclass.java
@@ -2,7 +2,7 @@
 public class myclass {
 
 	public static void main(String[] args) {
-		System.out.println("Myclass is called");
+		System.out.println("I am in sample WS - Myclass is called");
 
 	}
 
